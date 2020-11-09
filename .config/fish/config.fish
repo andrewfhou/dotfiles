c70a0e8e08444fe970ba4ba3ecb7c1b7d57f9e39
Andrew Hou <andrewfhou@gmail.com>
2020-11-09 09:58:52 -0500

    modify path for afh-pc

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -41,49 +41,7 @@ funcsave fish_greeting
 funcsave fuck
 
 # $PATH directories
-set -x PATH /home/mweepigeon/Software/ctf-tools/bin $PATH
-set -x PATH /home/mweepigeon/.cargo/bin $PATH
-set -x PATH /home/mweepigeon/.vim/bundle/vim-superman/bin $PATH
-
-# kdesrc-build initial setup
-# kdesrc-build directory path
-set -x PATH /home/mweepigeon/kde/src/kdesrc-build $PATH
-
-function kdesrc-run
-    source /home/mweepigeon/kde/build/$argv/prefix.sh; and /home/mweepigeon/kde/usr/bin/$argv
-end
-
-set NEVER_BUILD_LIST "Qt5 libdbusmenu-qt oxygen oxygen-icons5 kdegames akonadi telepathy-accounts-signon phonon kio-extras"
-set ONLY_BUILD_RARELY_LIST "modemmanager-qt networkmanager-qt bluez-qt khtml kdelibs4support gpgme"
-set PLASMA_PACKAGE_LIST "plasma-desktop plasma-workspace plasma-framework plasma-integration plasma-nm plasma-pa plasma-thunderbolt plasma-vault plasma-workspace-wallpapers kdeplasma-addons kwin systemsettings kscreen breeze discover kinfocenter kde-cli-tools print-manager plasma-sdk kirigami-gallery"
-
-function kb
-    set REPO (basename $PWD)
-    kdesrc-build $REPO --no-src --resume-from $REPO
-    set ERRORFILE ~/kde/src/log/latest/$REPO/error.log;
-    if test -f $ERRORFILE
-        cat $ERRORFILE
-    end
-end
-
-function kbdeps
-    set REPO (basename $PWD)
-    kdesrc-build $REPO --ignore-modules $NEVER_BUILD_LIST
-    set ERRORFILE ~/kde/src/log/latest/$REPO/error.log;
-    if test -f $ERRORFILE
-        cat $ERRORFILE
-    end
-end
-
-# BROKEN
-function build_plasma
-    kdesrc-build $PLASMA_PACKAGE_LIST --include-dependencies --ignore-modules $NEVER_BUILD_LIST $ONLY_BUILD_RARELY_LIST; and echo 'Installing Plasma Dev session...'; and sudo ~/kde/build/plasma-workspace/login-sessions/install-sessions.sh
-end
-
-# BROKEN
-function build_everything
-    kdesrc-build $PLASMA_PACKAGE_LIST dolphin konsole kate gwenview okular spectacle elisa kamoso kup --include-dependencies --ignore-modules $NEVER_BUILD_LIST; and sudo ~/kde/build/plasma-workspace/login-sessions/install-sessions.sh
-end
+set -x PATH /home/mweep/.vim/bundle/vim-superman/bin $PATH
 
 # set sagemath alias
 function sage
