d3e0a7e0b6c892c77833459b09835dd01d773a77
Andrew Hou <andrewfhou@gmail.com>
2026-05-10 21:49:34 -0400

    update

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,29 +1,15 @@
 set -g theme_title_use_abbreviated_path no
 set -g theme_complete_path yes
-set -gx EDITOR vim
-set -gx WINEARCH win32
-set -gx WINEPREFIX ~/.wine32
+set -gx EDITOR nvim
 
-thefuck --alias | source
+# fish startup greeting
+set -U fish_greeting ""
 
-# FISH GREETING (FORTUNE)
-function fish_greeting
-    if not type fortune > /dev/null 2>&1
-                apt-get install fortune
-    end
-    fortune -a
-end
+# set dotfiles alias
+alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
 
 function gri
     grep -ri $argv
-end
-
-# rsync backup commands
-function rsync_backup
-    sudo rsync -aAX --info=progress2 --delete --exclude-from="/home/mweepigeon/Documents/backup_exclusion_list" /home/mweepigeon /mnt/backups
-end
-function rsync_restore
-    sudo rsync -aAX --info=progress2 --delete --exclude-from="/home/mweepigeon/Documents/backup_exclusion_list" /mnt/backups /home/mweepigeon
 end
 
 # THE FUCK
@@ -37,87 +23,10 @@ function fuck -d "Correct your previous console command"
   end
 end
 
-funcsave fish_greeting
-funcsave fuck
-
-# $PATH directories
-
-set -x PATH /home/mweepigeon/Software/ctf-tools/bin $PATH
-set -x PATH /home/mweepigeon/.cargo/bin $PATH
-set -x PATH /home/mweepigeon/.vim/bundle/vim-superman/bin $PATH
-
-# cs 352 stuff
-
-# >>> JVM installed by coursier >>>
-set -x JAVA_HOME /home/mweepigeon/.cache/coursier/jvm/adopt@1.8.0-292
-set -x PATH /home/mweepigeon/.cache/coursier/jvm/adopt@1.8.0-292/bin $PATH
-# <<< JVM installed by coursier <<<
-
-# >>> coursier install directory >>>
-set -x PATH /home/mweepigeon/.local/share/coursier/bin $PATH
-# <<< coursier install directory <<<
-
-set -x PATH /home/mweepigeon/.local/share/coursier/bin $PATH
-
-# end cs 352 stuff
-
-# kdesrc-build initial setup
-# kdesrc-build directory path
-set -x PATH /home/mweepigeon/kde/src/kdesrc-build $PATH
-
-function kdesrc-run
-    source /home/mweepigeon/kde/build/$argv/prefix.sh; and /home/mweepigeon/kde/usr/bin/$argv
-end
-
-set NEVER_BUILD_LIST "Qt5 libdbusmenu-qt oxygen oxygen-icons5 kdegames akonadi telepathy-accounts-signon phonon kio-extras"
-set ONLY_BUILD_RARELY_LIST "modemmanager-qt networkmanager-qt bluez-qt khtml kdelibs4support gpgme"
-set PLASMA_PACKAGE_LIST "plasma-desktop plasma-workspace plasma-framework plasma-integration plasma-nm plasma-pa plasma-thunderbolt plasma-vault plasma-workspace-wallpapers kdeplasma-addons kwin systemsettings kscreen breeze discover kinfocenter kde-cli-tools print-manager plasma-sdk kirigami-gallery"
-
-function kb
-    set REPO (basename $PWD)
-    kdesrc-build $REPO --no-src --resume-from $REPO
-    set ERRORFILE ~/kde/src/log/latest/$REPO/error.log;
-    if test -f $ERRORFILE
-        cat $ERRORFILE
-    end
-end
-
-function kbdeps
-    set REPO (basename $PWD)
-    kdesrc-build $REPO --ignore-modules $NEVER_BUILD_LIST
-    set ERRORFILE ~/kde/src/log/latest/$REPO/error.log;
-    if test -f $ERRORFILE
-        cat $ERRORFILE
-    end
-end
-
-# BROKEN
-function build_plasma
-    kdesrc-build $PLASMA_PACKAGE_LIST --include-dependencies --ignore-modules $NEVER_BUILD_LIST $ONLY_BUILD_RARELY_LIST; and echo 'Installing Plasma Dev session...'; and sudo ~/kde/build/plasma-workspace/login-sessions/install-sessions.sh
-end
-
-# BROKEN
-function build_everything
-    kdesrc-build $PLASMA_PACKAGE_LIST dolphin konsole kate gwenview okular spectacle elisa kamoso kup --include-dependencies --ignore-modules $NEVER_BUILD_LIST; and sudo ~/kde/build/plasma-workspace/login-sessions/install-sessions.sh
-end
-
-# set sagemath alias
-function sage
-    /home/mweepigeon/Software/SageMath/sage
-end
-
-# set dotfiles alias
-alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
-
 # BOBTHEFISH SETTINGS
 set -g theme_display_user yes
 set -g theme_display_hostname yes
 set -g theme_color_scheme base16-dark
-
-# op25 alias
-function op25
-    bash /home/mweepigeon/Software/op25/op25.sh
-end
 
 # nvim alias
 alias vim='nvim'
